f8608efba92a2da342b0c151c8c2d3cb3254e3b6
William Ao <wtao@stu.naperville203.org>
2018-04-09 09:02:18 -0500

    jhvbj

--- recursiveIntro.java
+++ recursiveIntro.java
@@ -8,40 +8,84 @@
  */
 public class recursiveIntro
 {
-    public static void main(String[] args)
-    {
-        methodOne(10);
-        methodTwo(10);
-    }
-    public static void methodOne(int n)
-    {
-        System.out.println(n);
-        if (n>0)
-        {
-            methodOne(n-1);
-        }
-    }
-    public static void methodTwo(int n)
-    {
-        System.out.println(0);
-        if (n>0)
-        {
-            methodTwo(n-(n-1));
-        }
-    }
-    public static String methodThree(String string)
-    {
-        String reverse = new String(string);
-        
-        return reverse;
-    }
-    public static boolean methodFour(int num)
-    {
-        return true;
-    }
-    public static int methodFive()
-    {
-        return 5;
-    }
+      public static void main(String[] args)
+  {
+   
+         // nums(100);
+         //nums2(100);
+         //revString("waleed");
+    //       int n = 73;
+    //       int x = prime(n,2);
+    //        if(x == 1)
+    //         {
+    //             System.out.println(n+" is prime number");
+    //         }
+    //         else
+    //         {
+    //             System.out.println(n+" is not prime number");
+    //         }
+    System.out.print(fibb(12));
+  }
+  
+  public static void nums(int n)
+  {
+      if (n==0)
+      System.out.println("0");
+      else
+      {
+          System.out.println(n);
+          nums(n-1);    
+      }
+  }
+
+  public static void nums2(int n)
+   {
+       if (n==0)
+       System.out.println("0");
+       else
+       {
+           nums(n-1);   
+           System.out.println(n);
+       }
+   }
+  
+  public static void revString(String b)
+  {
+     if (b.length()-1 >= 0)
+     {
+     System.out.print(b.charAt(b.length()-1));
+     revString(b.substring(0,b.length() -1));
+     }
+   
+   }
+  
+  static int prime(int y,int i)
+   {
+        if(i < y)
+       {
+           if(y % i != 0) 
+           {
+               return(prime(y, ++i));
+           } 
+           else
+           {
+               return 0; 
+           }
+       }
+       return 1;
+   }
+   
+  static int fibb(int n)
+  {
+      if(n == 0)
+       return 0;
+      else if(n == 1)
+       return 1;
+      else
+      {
+       return fibb(n - 1) + fibb(n - 2);
+      }
+       
+  }
 
 }
